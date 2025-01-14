dddf69e991e4bc03ab6ce436e96c200cdaec195a
Deepraj Lodhi <deeprajlodhi59@gmail.com>
2025-01-14 10:30:48 +0530

    To find out the date, time and year

---
+++ Day-6/TimeZones.java
@@ -0,0 +1,23 @@
+import java.time.ZonedDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
+public class TimeZones {
+
+    public static void main(String[] args) {
+        // create a formatter for displaying date and time
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss z");
+
+        // Get the current time in gmt
+        ZonedDateTime gmtTime = ZonedDateTime.now(ZoneId.of("GMT"));
+        System.out.println("Current Time in GMT: " + gmtTime.format(formatter));
+
+        // Get the current time in IST
+        ZonedDateTime istTime = ZonedDateTime.now(ZoneId.of("Asia/Kolkata"));
+        System.out.println("Current Time in IST: " + istTime.format(formatter));
+
+        // Get the current time in PST
+        ZonedDateTime pstTime = ZonedDateTime.now(ZoneId.of("America/Los_Angeles"));
+        System.out.println("Current Time in PST: " + pstTime.format(formatter));
+    }
+}
